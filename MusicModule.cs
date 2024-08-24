8a679e702ada3d1c0b606ab4b4a1d7c7615b3d4e
Oskar ♂ <you@example.com>
2024-08-25 01:24:30 +0300

    button fix

--- MusicModule.cs
+++ MusicModule.cs
@@ -26,11 +26,6 @@ public sealed class MusicModule : InteractionModuleBase<SocketInteractionContext
         _guildDbContext = gachiDbContext;
     }
     #region Commands
-    /// <summary>
-    /// Adds link to users playlist.
-    /// </summary>
-    /// <param name="query">Track query</param>
-    /// <returns>a task that represents the asynchronous operation</returns>
     [SlashCommand("playlistadd", "Adds a playlist entry ", runMode: RunMode.Async)]
     public async Task AddTrackToPlaylist(string playlist, string query)
     {
@@ -73,11 +68,6 @@ public sealed class MusicModule : InteractionModuleBase<SocketInteractionContext
         }
     }
 
-    /// <summary>
-    ///     Plays all songs from specific playlist.
-    /// </summary>
-    /// <param name="playlist">Track query</param>
-    /// <returns>a task that represents the asynchronous operation</returns>
     [SlashCommand("pp", "Plays all songs from specific playlist", runMode: RunMode.Async)]
     public async Task PlayPlayList(string playlist)
     {
@@ -100,10 +90,6 @@ public sealed class MusicModule : InteractionModuleBase<SocketInteractionContext
         await FollowupAsync($"🔈 Playing ♂: {playlistSongs[0].Playlist} playlist").ConfigureAwait(false);
     }
 
-    /// <summary>
-    ///     Disconnects from the current voice channel connected to asynchronously.
-    /// </summary>
-    /// <returns>a task that represents the asynchronous operation</returns>
     [SlashCommand("disconnect", "Disconnects from the current voice channel connected to", runMode: RunMode.Async)]
     public async Task Disconnect()
     {
@@ -113,11 +99,6 @@ public sealed class MusicModule : InteractionModuleBase<SocketInteractionContext
         await RespondAsync("Disconnected.").ConfigureAwait(false);
     }
 
-    /// <summary>
-    /// Changes the playback speed of the current track.
-    /// </summary>
-    /// <param name="speed">The playback speed (0.5 - 3.0)</param>
-    /// <returns>A task that represents the asynchronous operation.</returns>
     [SlashCommand("speed", description: "Changes the playback speed (0.5 - 3.0)", runMode: RunMode.Async)]
     public async Task ChangeSpeed(double speed)
     {
@@ -145,11 +126,6 @@ public sealed class MusicModule : InteractionModuleBase<SocketInteractionContext
         await RespondAsync($"Playback speed set to {speed}x.").ConfigureAwait(false);
     }
 
-    /// <summary>
-    ///     Plays music asynchronously.
-    /// </summary>
-    /// <param name="query">the search query</param>
-    /// <returns>a task that represents the asynchronous operation</returns>
     [SlashCommand("play", description: "Plays music", runMode: RunMode.Async)]
     public async Task Play(string query)
     {
@@ -209,10 +185,6 @@ public sealed class MusicModule : InteractionModuleBase<SocketInteractionContext
         }
     }
 
-    /// <summary>
-    ///     Plays gachi radio.
-    /// </summary>
-    /// <returns>a task that represents the asynchronous operation</returns>
     [SlashCommand("radio", description: "Plays gachi radio", runMode: RunMode.Async)]
     public async Task Radio()
     {
@@ -236,59 +208,15 @@ public sealed class MusicModule : InteractionModuleBase<SocketInteractionContext
         }
     }
 
-    /// <summary>
-    ///     Shows the track position asynchronously.
-    /// </summary>
-    /// <returns>a task that represents the asynchronous operation</returns>
-    [SlashCommand("position", description: "Shows the track position", runMode: RunMode.Async)]
-    public async Task Position()
-    {
-        var player = await GetPlayerAsync(connectToVoiceChannel: false).ConfigureAwait(false);
-
-        if (player is null)
-        {
-            return;
-        }
-
-        if (player.CurrentItem is null)
-        {
-            await RespondAsync("Nothing playing!").ConfigureAwait(false);
-            return;
-        }
-
-        await RespondAsync($"Position: {player.Position?.Position} / {player.CurrentTrack.Duration}.").ConfigureAwait(false);
-    }
-
-    /// <summary>
-    ///     Stops the current track asynchronously.
-    /// </summary>
-    /// <returns>a task that represents the asynchronous operation</returns>
     [SlashCommand("stop", description: "Stops the current track", runMode: RunMode.Async)]
     public async Task Stop()
     {
         var player = await GetPlayerAsync(connectToVoiceChannel: false);
-
-        if (player is null)
-        {
-            return;
-        }
-
-        if (player.CurrentItem is null)
-        {
-            await RespondAsync("Nothing playing!").ConfigureAwait(false);
-            return;
-        }
-
+        if (player.CurrentItem is null || player is null) return;
         await player.StopAsync().ConfigureAwait(false);
         await player.DisconnectAsync().ConfigureAwait(false);
-        await RespondAsync("Stopped playing.").ConfigureAwait(false);
     }
 
-    /// <summary>
-    ///     Updates the player volume asynchronously.
-    /// </summary>
-    /// <param name="volume">the volume (1 - 1000)</param>
-    /// <returns>a task that represents the asynchronous operation</returns>
     [SlashCommand("volume", description: "Sets the player volume (0 - 1000%)", runMode: RunMode.Async)]
     public async Task Volume(int volume = 100)
     {
@@ -313,70 +241,24 @@ public sealed class MusicModule : InteractionModuleBase<SocketInteractionContext
     public async Task Skip()
     {
         var player = await GetPlayerAsync(connectToVoiceChannel: false);
-
-        if (player is null)
-        {
-            return;
-        }
-
-        if (player.CurrentItem is null)
-        {
-            await RespondAsync("Nothing playing!").ConfigureAwait(false);
-            return;
-        }
-
+        if (player.CurrentItem is null || player is null) return;
         await player.SkipAsync().ConfigureAwait(false);
-
-        var track = player.CurrentItem;
-
-        if (track is not null)
-        {
-            await RespondAsync($"Skipped. Now playing: {track.Track!.Title}").ConfigureAwait(false);
-        }
-        else
-        {
-            await RespondAsync("Skipped. Stopped playing because the queue is now empty.").ConfigureAwait(false);
-        }
     }
 
     [SlashCommand("pause", description: "Pauses the player.", runMode: RunMode.Async)]
     public async Task PauseAsync()
     {
         var player = await GetPlayerAsync(connectToVoiceChannel: false);
-
-        if (player is null)
-        {
-            return;
-        }
-
-        if (player.State is PlayerState.Paused)
-        {
-            await RespondAsync("Player is already paused.").ConfigureAwait(false);
-            return;
-        }
-
+        if (player.State is PlayerState || player is null) return;
         await player.PauseAsync().ConfigureAwait(false);
-        await RespondAsync("Paused.").ConfigureAwait(false);
     }
 
     [SlashCommand("resume", description: "Resumes the player.", runMode: RunMode.Async)]
     public async Task ResumeAsync()
     {
         var player = await GetPlayerAsync(connectToVoiceChannel: false);
-
-        if (player is null)
-        {
-            return;
-        }
-
-        if (player.State is not PlayerState.Paused)
-        {
-            await RespondAsync("Player is not paused.").ConfigureAwait(false);
-            return;
-        }
-
+        if (player.State is not PlayerState.Paused || player is null) return;
         await player.ResumeAsync().ConfigureAwait(false);
-        await RespondAsync("Resumed.").ConfigureAwait(false);
     }
     #endregion
     #region Buttons
@@ -384,127 +266,68 @@ public sealed class MusicModule : InteractionModuleBase<SocketInteractionContext
     public async Task HandlePauseButton()
     {
         await DeferAsync().ConfigureAwait(false);
-
         var player = await GetPlayerAsync(connectToVoiceChannel: false).ConfigureAwait(false);
-
-        if (player is null)
-        {
-            //await ModifyOriginalResponseAsync(msg => msg.Content = "No player found!").ConfigureAwait(false);
-            return;
-        }
-
+        if (player is null) return;
         await player.PauseAsync().ConfigureAwait(false);
-
-        new ComponentBuilder()
+        var components = new ComponentBuilder()
             .WithButton("Resume", "resume_button", ButtonStyle.Primary)
             .WithButton("Skip", "skip_button", ButtonStyle.Secondary)
             .WithButton("Repeat", "repeat_button", ButtonStyle.Primary)
             .WithButton("Stop", "stop_button", ButtonStyle.Danger).Build();
 
-        //await ModifyOriginalResponseAsync(msg =>
-        //{
-        //    msg.Content = "Paused.";
-        //    msg.Components = builder.Build();
-        //}).ConfigureAwait(false);
+        var message = await GetOriginalResponseAsync();
+        await message.ModifyAsync(msg => msg.Components = components).ConfigureAwait(false);
     }
 
     [ComponentInteraction("resume_button")]
     public async Task HandleResumeButton()
     {
         await DeferAsync().ConfigureAwait(false);
-
         var player = await GetPlayerAsync(connectToVoiceChannel: false).ConfigureAwait(false);
-
-        if (player is null)
-        {
-            //await ModifyOriginalResponseAsync(msg => msg.Content = "No player found!").ConfigureAwait(false);
-            return;
-        }
+        if (player is null) return;
 
         await player.ResumeAsync().ConfigureAwait(false);
-
-        new ComponentBuilder()
+        var components = new ComponentBuilder()
             .WithButton("Pause", "pause_button", ButtonStyle.Primary)
             .WithButton("Skip", "skip_button", ButtonStyle.Secondary)
             .WithButton("Repeat", "repeat_button", ButtonStyle.Primary)
             .WithButton("Stop", "stop_button", ButtonStyle.Danger).Build();
-        //await ModifyOriginalResponseAsync(msg =>
-        //{
-        //    msg.Content = "Resumed.";
-        //    msg.Components = builder.Build();
-        //}).ConfigureAwait(false);
+        var message = await GetOriginalResponseAsync();
+        await message.ModifyAsync(msg => msg.Components = components).ConfigureAwait(false);
     }
 
     [ComponentInteraction("skip_button")]
     public async Task HandleSkipButton()
     {
         var player = await GetPlayerAsync(connectToVoiceChannel: false).ConfigureAwait(false);
-
-        if (player is null)
-        {
-            await RespondAsync("No player found!").ConfigureAwait(false);
-            return;
-        }
-
+        if (player is null) return;
         await player.SkipAsync().ConfigureAwait(false);
-
-        await RespondAsync("Skipped the current track.").ConfigureAwait(false);
     }
 
     [ComponentInteraction("stop_button")]
     public async Task HandleStopButton()
     {
         var player = await GetPlayerAsync(connectToVoiceChannel: false).ConfigureAwait(false);
-
-        if (player is null)
-        {
-            await RespondAsync("No player found!").ConfigureAwait(false);
-            return;
-        }
-
+        if (player is null) return;
         await player.StopAsync().ConfigureAwait(false);
-
-        await RespondAsync("Stopped the player.").ConfigureAwait(false);
     }
 
     [ComponentInteraction("repeat_button")]
     public async Task HandleRepeatButton()
     {
         await DeferAsync().ConfigureAwait(false);
-
         var player = await GetPlayerAsync(connectToVoiceChannel: false).ConfigureAwait(false);
-
-        if (player is null)
-        {
-            //await ModifyOriginalResponseAsync(msg => msg.Content = "No player found!").ConfigureAwait(false);
-            return;
-        }
-
+        if (player is null) return;
+        await player.ResumeAsync().ConfigureAwait(false);
         player.RepeatMode = player.RepeatMode == TrackRepeatMode.Track ? TrackRepeatMode.None : TrackRepeatMode.Track;
         new ComponentBuilder()
             .WithButton("Pause", "pause_button", ButtonStyle.Primary)
             .WithButton("Skip", "skip_button", ButtonStyle.Secondary)
             .WithButton(player.RepeatMode == TrackRepeatMode.Track ? "Stop Repeating" : "Repeat", "repeat_button", ButtonStyle.Primary)
             .WithButton("Stop", "stop_button", ButtonStyle.Danger).Build();
-        
-        //var repeatStatus = player.RepeatMode == TrackRepeatMode.Track ? "Repeat mode enabled." : "Repeat mode disabled.";
-        //await ModifyOriginalResponseAsync(msg =>
-        //{
-        //    msg.Content = repeatStatus;
-        //    msg.Components = builder.Build();
-        //}).ConfigureAwait(false);
     }
     #endregion
     #region Helpers
-    /// <summary>
-    ///     Gets the guild player asynchronously.
-    /// </summary>
-    /// <param name="connectToVoiceChannel">
-    ///     a value indicating whether to connect to a voice channel
-    /// </param>
-    /// <returns>
-    ///     a task that represents the asynchronous operation. The task result is the lavalink player.
-    /// </returns>
     private async ValueTask<VoteLavalinkPlayer?> GetPlayerAsync(bool connectToVoiceChannel = true)
     {
         var retrieveOptions = new PlayerRetrieveOptions(
