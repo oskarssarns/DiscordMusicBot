6c9dc99b0641f2c8f438c92010ddd6686d20d9b8
Oskars <you@example.com>
2025-07-13 00:27:05 +0300

    fix

--- DiscordClientHost.cs
+++ DiscordClientHost.cs
@@ -1,195 +1,135 @@
-﻿using Discord;
-using Discord.Interactions;
-using Discord.WebSocket;
-using Lavalink4NET;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System.Reflection;
-using LavaLinkLouieBot.Helpers;
+﻿namespace LavaLinkLouieBot;
 
-namespace LavaLinkLouieBot
+internal sealed class DiscordClientHost : IHostedService, IDisposable
 {
-    internal sealed class DiscordClientHost : IHostedService, IDisposable
+    private readonly DiscordSocketClient _discordSocketClient;
+    private readonly InteractionService _interactionService;
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<DiscordClientHost> _logger;
+    private readonly IAudioService _audioService;
+    private Timer _timer;
+    private TaskCompletionSource<bool> _readyTcs;
+
+    public DiscordClientHost(
+        DiscordSocketClient discordSocketClient,
+        InteractionService interactionService,
+        IServiceProvider serviceProvider,
+        IConfiguration configuration,
+        ILogger<DiscordClientHost> logger,
+        IAudioService audioService)
     {
-        private readonly DiscordSocketClient _discordSocketClient;
-        private readonly InteractionService _interactionService;
-        private readonly IServiceProvider _serviceProvider;
-        private readonly IConfiguration _configuration;
-        private readonly ILogger<DiscordClientHost> _logger;
-        private readonly IAudioService _audioService;
-        private Timer _timer;
-        private TaskCompletionSource<bool> _readyTcs;
+        ArgumentNullException.ThrowIfNull(discordSocketClient);
+        ArgumentNullException.ThrowIfNull(interactionService);
+        ArgumentNullException.ThrowIfNull(serviceProvider);
+        ArgumentNullException.ThrowIfNull(configuration);
+        ArgumentNullException.ThrowIfNull(logger);
+        ArgumentNullException.ThrowIfNull(audioService);
 
-        public DiscordClientHost(
-            DiscordSocketClient discordSocketClient,
-            InteractionService interactionService,
-            IServiceProvider serviceProvider,
-            IConfiguration configuration,
-            ILogger<DiscordClientHost> logger,
-            IAudioService audioService)
+        _discordSocketClient = discordSocketClient;
+        _interactionService = interactionService;
+        _serviceProvider = serviceProvider;
+        _configuration = configuration;
+        _logger = logger;
+        _audioService = audioService;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _readyTcs = new TaskCompletionSource<bool>();
+        _discordSocketClient.InteractionCreated += InteractionCreated;
+        _discordSocketClient.Ready += ClientReady;
+
+        _logger.LogInformation("Starting Discord client...");
+        await _discordSocketClient
+            .LoginAsync(TokenType.Bot, _configuration["BotToken"])
+            .ConfigureAwait(false);
+
+        await _discordSocketClient
+            .StartAsync()
+            .ConfigureAwait(false);
+
+        _logger.LogInformation("Waiting for Discord client to be ready...");
+        try
         {
-            ArgumentNullException.ThrowIfNull(discordSocketClient);
-            ArgumentNullException.ThrowIfNull(interactionService);
-            ArgumentNullException.ThrowIfNull(serviceProvider);
-            ArgumentNullException.ThrowIfNull(configuration);
-            ArgumentNullException.ThrowIfNull(logger);
-            ArgumentNullException.ThrowIfNull(audioService);
-
-            _discordSocketClient = discordSocketClient;
-            _interactionService = interactionService;
-            _serviceProvider = serviceProvider;
-            _configuration = configuration;
-            _logger = logger;
-            _audioService = audioService;
+            await _readyTcs.Task.WaitAsync(TimeSpan.FromSeconds(180), cancellationToken);
+            _logger.LogInformation("Discord client is ready.");
         }
-
-        public async Task StartAsync(CancellationToken cancellationToken)
+        catch (TimeoutException ex)
         {
-            _readyTcs = new TaskCompletionSource<bool>();
-            _discordSocketClient.InteractionCreated += InteractionCreated;
-            _discordSocketClient.Ready += ClientReady;
-
-            _logger.LogInformation("Starting Discord client...");
-            await _discordSocketClient
-                .LoginAsync(TokenType.Bot, _configuration["BotToken"])
-                .ConfigureAwait(false);
-
-            await _discordSocketClient
-                .StartAsync()
-                .ConfigureAwait(false);
-
-            _logger.LogInformation("Waiting for Discord client to be ready...");
-            try
-            {
-                await _readyTcs.Task.WaitAsync(TimeSpan.FromSeconds(180), cancellationToken);
-                _logger.LogInformation("Discord client is ready.");
-            }
-            catch (TimeoutException ex)
-            {
-                _logger.LogError(ex, "Timed out while waiting for Discord client being ready.");
-                throw;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "An error occurred while waiting for Discord client being ready.");
-                throw;
-            }
-
-            _timer = new Timer(CheckAndReconnectLavalink, null, 0, (int)TimeSpan.FromMinutes(5).TotalMilliseconds);
+            _logger.LogError(ex, "Timed out while waiting for Discord client being ready.");
+            throw;
         }
-
-        public async Task StopAsync(CancellationToken cancellationToken)
+        catch (Exception ex)
         {
-            _discordSocketClient.InteractionCreated -= InteractionCreated;
-            _discordSocketClient.Ready -= ClientReady;
-
-            await _discordSocketClient
-                .StopAsync()
-                .ConfigureAwait(false);
-
-            _timer?.Change(Timeout.Infinite, 0);
-            _timer?.Dispose();
+            _logger.LogError(ex, "An error occurred while waiting for Discord client being ready.");
+            throw;
         }
+    }
 
-        private Task InteractionCreated(SocketInteraction interaction)
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        _discordSocketClient.InteractionCreated -= InteractionCreated;
+        _discordSocketClient.Ready -= ClientReady;
+
+        await _discordSocketClient
+            .StopAsync()
+            .ConfigureAwait(false);
+
+        _timer?.Change(Timeout.Infinite, 0);
+        _timer?.Dispose();
+    }
+
+    private Task InteractionCreated(SocketInteraction interaction)
+    {
+        var interactionContext = new SocketInteractionContext(_discordSocketClient, interaction);
+        return _interactionService.ExecuteCommandAsync(interactionContext, _serviceProvider);
+    }
+
+    private async Task ClientReady()
+    {
+        _readyTcs.SetResult(true);
+
+        await _interactionService
+            .AddModulesAsync(Assembly.GetExecutingAssembly(), _serviceProvider)
+            .ConfigureAwait(false);
+
+        await _interactionService
+            .RegisterCommandsToGuildAsync(ulong.Parse(_configuration["Server2"]))
+            .ConfigureAwait(false);
+        await _interactionService
+            .RegisterCommandsToGuildAsync(ulong.Parse(_configuration["Server1"]))
+            .ConfigureAwait(false);
+        await _interactionService
+            .RegisterCommandsToGuildAsync(ulong.Parse(_configuration["Server3"]))
+            .ConfigureAwait(false);
+        await _interactionService
+            .RegisterCommandsToGuildAsync(ulong.Parse(_configuration["Server4"]))
+            .ConfigureAwait(false);
+    }
+
+    private async Task<LavaLinkLouieBot.Helpers.LavalinkServerConfig> GetLavalinkServerConfiguration(IConfiguration configuration)
+    {
+        List<LavaLinkLouieBot.Helpers.LavalinkServer> servers = await LavaLinkHelper.GetLavalinkServers(configuration["LavaLinkSource"]);
+        List<LavaLinkLouieBot.Helpers.LavalinkServer> onlineServers = await LavaLinkHelper.GetOnlineLavalinkServers(servers, configuration["TestQuery"], configuration);
+        if (onlineServers.Count > 0)
         {
-            var interactionContext = new SocketInteractionContext(_discordSocketClient, interaction);
-            return _interactionService.ExecuteCommandAsync(interactionContext, _serviceProvider);
+            var server = onlineServers[0];
+            string scheme = server.Secure.ToLower() == "true" ? "https" : "http";
+            return new LavaLinkLouieBot.Helpers.LavalinkServerConfig
+            {
+                BaseAddress = $"{scheme}://{server.Host}:{server.Port}",
+                Passphrase = server.Password
+            };
         }
-
-        private async Task ClientReady()
+        else
         {
-            _readyTcs.SetResult(true);
-
-            await _interactionService
-                .AddModulesAsync(Assembly.GetExecutingAssembly(), _serviceProvider)
-                .ConfigureAwait(false);
-
-            //await _interactionService
-            //    .RegisterCommandsToGuildAsync(ulong.Parse(_configuration["Server2"]))
-            //    .ConfigureAwait(false);
-            //await _interactionService
-            //    .RegisterCommandsToGuildAsync(ulong.Parse(_configuration["Server1"]))
-            //    .ConfigureAwait(false);
-            //await _interactionService
-            //    .RegisterCommandsToGuildAsync(ulong.Parse(_configuration["Server3"]))
-            //    .ConfigureAwait(false);
-            await _interactionService
-                .RegisterCommandsToGuildAsync(ulong.Parse(_configuration["Server4"]))
-                .ConfigureAwait(false);
+            throw new InvalidOperationException("No online servers found.");
         }
+    }
 
-        private async void CheckAndReconnectLavalink(object state)
-        {
-            try
-            {
-                bool isPlayingMusic = await CheckLavalinkMusicStatusAsync();
-                if (!isPlayingMusic)
-                {
-                    _logger.LogInformation("Music playback issue detected. Reconnecting to a new Lavalink server.");
-                    await ReconnectLavalinkAsync();
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error while checking Lavalink server status.");
-            }
-        }
-
-        private async Task<bool> CheckLavalinkMusicStatusAsync()
-        {
-            try
-            {
-                var servers = await LavaLinkHelper.GetLavalinkServers(_configuration["LavaLinkSource"]);
-                var onlineServers = await LavaLinkHelper.GetOnlineLavalinkServers(servers, _configuration["TestQuery"], _configuration);
-                return onlineServers.Count > 0;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error while checking Lavalink music status.");
-                return false;
-            }
-        }
-
-        private async Task ReconnectLavalinkAsync()
-        {
-            try
-            {
-                var newConfig = await GetLavalinkServerConfiguration(_configuration);
-                _logger.LogInformation("Reconnecting to Lavalink server: {BaseAddress}", newConfig.BaseAddress);
-                // Note: Lavalink4NET v4.0.27 does not support direct restart. Consider updating or handling reconnection differently.
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to reconnect to Lavalink server.");
-            }
-        }
-
-        private async Task<LavaLinkLouieBot.Helpers.LavalinkServerConfig> GetLavalinkServerConfiguration(IConfiguration configuration)
-        {
-            List<LavaLinkLouieBot.Helpers.LavalinkServer> servers = await LavaLinkHelper.GetLavalinkServers(configuration["LavaLinkSource"]);
-            List<LavaLinkLouieBot.Helpers.LavalinkServer> onlineServers = await LavaLinkHelper.GetOnlineLavalinkServers(servers, configuration["TestQuery"], configuration);
-            if (onlineServers.Count > 0)
-            {
-                var server = onlineServers[0];
-                string scheme = server.Secure.ToLower() == "true" ? "https" : "http";
-                return new LavaLinkLouieBot.Helpers.LavalinkServerConfig
-                {
-                    BaseAddress = $"{scheme}://{server.Host}:{server.Port}",
-                    Passphrase = server.Password
-                };
-            }
-            else
-            {
-                throw new InvalidOperationException("No online servers found.");
-            }
-        }
-
-        public void Dispose()
-        {
-            _timer?.Dispose();
-        }
+    public void Dispose()
+    {
+        _timer?.Dispose();
     }
 }
--- Helpers/LavaLinkHelper.cs
+++ Helpers/LavaLinkHelper.cs
@@ -1,156 +1,165 @@
-﻿using Lavalink4NET;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Sockets;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿namespace LavaLinkLouieBot.Helpers;
 
-namespace LavaLinkLouieBot.Helpers
+public static class LavaLinkHelper
 {
-    public static class LavaLinkHelper
+    public static async Task<List<LavalinkServer>> GetLavalinkServers(string source)
     {
-        public static async Task<List<LavalinkServer>> GetLavalinkServers(string source)
+        using HttpClient client = new HttpClient();
+
+        client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+
+        HttpResponseMessage response = await client.GetAsync(source);
+        response.EnsureSuccessStatusCode();
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+        JObject jsonResponse = JObject.Parse(responseBody);
+
+        List<LavalinkServer> servers = new List<LavalinkServer>();
+        bool skipFirst = true;
+
+        foreach (var doc in jsonResponse["docs"])
         {
-            using HttpClient client = new HttpClient();
-
-            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
-
-            HttpResponseMessage response = await client.GetAsync(source);
-            response.EnsureSuccessStatusCode();
-
-            string responseBody = await response.Content.ReadAsStringAsync();
-            JObject jsonResponse = JObject.Parse(responseBody);
-
-            List<LavalinkServer> servers = new List<LavalinkServer>();
-            bool skipFirst = true;
-
-            foreach (var doc in jsonResponse["docs"])
+            string text = doc["text"].ToString();
+            if (text.Contains("Host :"))
             {
-                string text = doc["text"].ToString();
-                if (text.Contains("Host :"))
+                if (skipFirst)
                 {
-                    if (skipFirst)
+                    skipFirst = false;
+                    continue;
+                }
+
+                LavalinkServer server = new LavalinkServer();
+
+                int hostStart = text.IndexOf("Host :") + 7;
+                int hostEnd = text.IndexOf('\n', hostStart);
+                server.Host = text.Substring(hostStart, hostEnd - hostStart).Trim();
+
+                int portStart = text.IndexOf("Port :") + 7;
+                int portEnd = text.IndexOf('\n', portStart);
+                server.Port = text.Substring(portStart, portEnd - portStart).Trim();
+
+                int passwordStart = text.IndexOf("Password :") + 11;
+                int passwordEnd = text.IndexOf('\n', passwordStart);
+                server.Password = text.Substring(passwordStart, passwordEnd - passwordStart).Trim();
+                server.Password = RemoveQuotes(server.Password);
+
+                int secureStart = text.IndexOf("Secure :") + 9;
+                int secureEnd = text.IndexOf('\n', secureStart);
+                server.Secure = text.Substring(secureStart, secureEnd - secureStart).Trim();
+
+                Match versionMatch = Regex.Match(text, @"Version\s*(\d+\.\d+\.\d+)");
+                if (versionMatch.Success)
+                {
+                    server.Version = versionMatch.Groups[1].Value;
+                    if (Version.Parse(server.Version) >= Version.Parse("4.0.0"))
                     {
-                        skipFirst = false;
-                        continue;
-                    }
-
-                    LavalinkServer server = new LavalinkServer();
-
-                    int hostStart = text.IndexOf("Host :") + 7;
-                    int hostEnd = text.IndexOf('\n', hostStart);
-                    server.Host = text.Substring(hostStart, hostEnd - hostStart).Trim();
-
-                    int portStart = text.IndexOf("Port :") + 7;
-                    int portEnd = text.IndexOf('\n', portStart);
-                    server.Port = text.Substring(portStart, portEnd - portStart).Trim();
-
-                    int passwordStart = text.IndexOf("Password :") + 11;
-                    int passwordEnd = text.IndexOf('\n', passwordStart);
-                    server.Password = text.Substring(passwordStart, passwordEnd - passwordStart).Trim();
-                    server.Password = RemoveQuotes(server.Password);
-
-                    int secureStart = text.IndexOf("Secure :") + 9;
-                    int secureEnd = text.IndexOf('\n', secureStart);
-                    server.Secure = text.Substring(secureStart, secureEnd - secureStart).Trim();
-
-                    Match versionMatch = Regex.Match(text, @"Version\s*(\d+\.\d+\.\d+)");
-                    if (versionMatch.Success)
-                    {
-                        server.Version = versionMatch.Groups[1].Value;
-                        if (Version.Parse(server.Version) >= Version.Parse("4.0.0"))
-                        {
-                            servers.Add(server);
-                        }
+                        servers.Add(server);
                     }
                 }
             }
-
-            return servers;
         }
 
-        public static async Task<bool> IsServerOnline(LavalinkServer server)
-        {
-            try
-            {
-                using TcpClient tcpClient = new TcpClient();
-                await tcpClient.ConnectAsync(server.Host, int.Parse(server.Port));
-                return tcpClient.Connected;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        public static async Task<bool> CanServerPlayMusic(LavalinkServer server, string testQuery, IConfiguration configuration)
-        {
-            try
-            {
-                var services = new ServiceCollection();
-                services.AddSingleton<IConfiguration>(configuration);
-                services.AddSingleton<DiscordSocketClient>();
-                services.AddSingleton<IDiscordClientWrapper, DiscordClientWrapper>();
-                services.AddLavalink();
-
-                string scheme = server.Secure.ToLower() == "true" ? "https" : "http";
-                string baseAddress = $"{scheme}://{server.Host}:{server.Port}";
-
-                services.ConfigureLavalink(options =>
-                {
-                    options.BaseAddress = new Uri(baseAddress);
-                    options.Passphrase = server.Password;
-                });
-
-                var serviceProvider = services.BuildServiceProvider();
-                var audioService = serviceProvider.GetRequiredService<IAudioService>();
-
-                var track = await audioService.Tracks.LoadTrackAsync(testQuery, TrackSearchMode.YouTube).ConfigureAwait(false);
-                return track != null;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        public static async Task<List<LavalinkServer>> GetOnlineLavalinkServers(List<LavalinkServer> servers, string testQuery, IConfiguration configuration)
-        {
-            List<LavalinkServer> onlineServers = new List<LavalinkServer>();
-
-            foreach (var server in servers)
-            {
-                if (await IsServerOnline(server) && await CanServerPlayMusic(server, testQuery, configuration))
-                {
-                    onlineServers.Add(server);
-                }
-            }
-
-            return onlineServers;
-        }
-
-        public static string RemoveQuotes(string input)
-        {
-            if (string.IsNullOrEmpty(input)) return input;
-            return input.Trim('"');
-        }
+        return servers;
     }
 
-    public class LavalinkServer
+    public static async Task<bool> IsServerOnline(LavalinkServer server)
     {
-        public string Host { get; set; }
-        public string Port { get; set; }
-        public string Password { get; set; }
-        public string Secure { get; set; }
-        public string Version { get; set; }
+        try
+        {
+            using TcpClient tcpClient = new TcpClient();
+            await tcpClient.ConnectAsync(server.Host, int.Parse(server.Port));
+            return tcpClient.Connected;
+        }
+        catch
+        {
+            return false;
+        }
     }
 
-    public class LavalinkServerConfig
+    public static async Task<bool> CanServerPlayMusic(LavalinkServer server, string testQuery, IConfiguration configuration)
     {
-        public string BaseAddress { get; set; }
-        public string Passphrase { get; set; }
+        try
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(configuration);
+            services.AddSingleton<DiscordSocketClient>();
+            services.AddSingleton<IDiscordClientWrapper, DiscordClientWrapper>();
+            services.AddLavalink();
+
+            string scheme = server.Secure.ToLower() == "true" ? "https" : "http";
+            string baseAddress = $"{scheme}://{server.Host}:{server.Port}";
+
+            services.ConfigureLavalink(options =>
+            {
+                options.BaseAddress = new Uri(baseAddress);
+                options.Passphrase = server.Password;
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+            var audioService = serviceProvider.GetRequiredService<IAudioService>();
+
+            var track = await audioService.Tracks.LoadTrackAsync(testQuery, TrackSearchMode.YouTube).ConfigureAwait(false);
+            return track != null;
+        }
+        catch
+        {
+            return false;
+        }
     }
+
+    public static async Task<List<LavalinkServer>> GetOnlineLavalinkServers(List<LavalinkServer> servers, string testQuery, IConfiguration configuration)
+    {
+        List<LavalinkServer> onlineServers = new List<LavalinkServer>();
+
+        foreach (var server in servers)
+        {
+            if (await IsServerOnline(server) && await CanServerPlayMusic(server, testQuery, configuration))
+            {
+                onlineServers.Add(server);
+            }
+        }
+
+        return onlineServers;
+    }
+
+    public static string RemoveQuotes(string input)
+    {
+        if (string.IsNullOrEmpty(input)) return input;
+        return input.Trim('"');
+    }
+
+    public static async Task<LavalinkServerConfig> GetLavalinkServerConfiguration(IConfiguration configuration)
+    {
+        List<LavaLinkLouieBot.Helpers.LavalinkServer> servers = await LavaLinkHelper.GetLavalinkServers(configuration["LavaLinkSource"]);
+        List<LavaLinkLouieBot.Helpers.LavalinkServer> onlineServers = await LavaLinkHelper.GetOnlineLavalinkServers(servers, configuration["TestQuery"], configuration);
+        if (onlineServers.Count > 0)
+        {
+            var server = onlineServers[0];
+            string scheme = server.Secure.ToLower() == "true" ? "https" : "http";
+            return new LavaLinkLouieBot.Helpers.LavalinkServerConfig
+            {
+                BaseAddress = $"{scheme}://{server.Host}:{server.Port}",
+                Passphrase = server.Password
+            };
+        }
+        else
+        {
+            throw new InvalidOperationException("No online servers found.");
+        }
+    }
+}
+
+public class LavalinkServer
+{
+    public string Host { get; set; }
+    public string Port { get; set; }
+    public string Password { get; set; }
+    public string Secure { get; set; }
+    public string Version { get; set; }
+}
+
+public class LavalinkServerConfig
+{
+    public string BaseAddress { get; set; }
+    public string Passphrase { get; set; }
 }
